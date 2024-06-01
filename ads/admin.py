9bfa29b367402afa45119e580d47b15145b5aee2
SanorSmith <141268934+SanorSmith@users.noreply.github.com>
2024-06-01 16:51:18 +0000

    Migrate Userprofile and Service to the database

--- ads/admin.py
+++ ads/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import UserProfile, Service
 
-# Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(Service)
--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class UserProfile(models.Model):
+    name = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    contact_info = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.name.username
+
+class Service(models.Model):
+    ads_author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    occupation = models.CharField(max_length=100)
+    contact_info = models.CharField(max_length=255)
+    featured_image = CloudinaryField('image', default='placeholder')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
